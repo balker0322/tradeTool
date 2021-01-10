189ae463428c837e77cf119e251285640967c6cc
inspiron5559 <bryogenin_haul@aleeas.com>
2021-01-10 18:29:41 +0800

    Refactor Task() class

--- model/task.py
+++ model/task.py
@@ -1,20 +1,20 @@
-
+import time
 
 info_list = [
-    "id",
+    "task_id",
+    "create_task_timestamp",
+    "end_task_timestamp",
+    "reward",
     "pair",
-    "quantity",
-    "take_profit_val",
-    "take_profit_sell_order_id"
-    "stop_loss_val",
-    "stop_loss_sell_order_id",
-    "entry_price_val",
-    "entry_buy_order_id",
-    "position_size_val",
-    "risk_percent_val",
+    "buy_price",
+    "sell_price",
+    "position_size",
+    "profit",
+    "buy_order_id",
+    "take_profit_order_id",
+    "stop_loss_order_id",
     "status",
     "next_step",
-    "timestamp",
 ]
 
 class Task():
@@ -25,73 +25,114 @@ class Task():
         for info in info_list:
             self.task_info[info] = ""
     
-    def set_symbol(self, symbol):
-        self.task_info["symbol"] = symbol
 
-    def set_quantity(self, quantity):
-        self.task_info["quantity"] = quantity
-
-    def set_take_profit_val(self, take_profit_val):
-        self.task_info["take_profit_val"] = take_profit_val
-
-    def set_stop_loss_val(self, stop_loss_val):
-        self.task_info["stop_loss_val"] = stop_loss_val
-
-    def set_entry_price_val(self, entry_price_val):
-        self.task_info["entry_price_val"] = entry_price_val
-
-    def set_position_size_val(self, position_size_val):
-        self.task_info["position_size_val"] = position_size_val
-
-    def set_risk_percent_val(self, risk_percent_val):
-        self.task_info["risk_percent_val"] = risk_percent_val
-
-    def set_status(self, status):
-        self.task_info["status"] = status
-
-    def set_next_step(self, next_step):
-        self.task_info["next_step"] = next_step
-
-    def set_timestamp(self, timestamp):
-        self.task_info["timestamp"] = timestamp
-    
     def set_task_to_active(self):
+
+        self.set_create_task_timestamp(
+            self.__get_timestamp()
+        )
+
         self.set_status('ACTIVE')
-    
+
+
     def set_task_to_inactive(self):
+
+        self.set_end_task_timestamp(
+            self.__get_timestamp()
+        )
+
         self.set_status('INACTIVE')
 
+    
+    def __get_timestamp(self):
+        return str(int(round(time.time() * 1000)))
 
-    def get_id(self):
-        return self.task_info["id"]
+    def set_task_id(self, task_id):
+        self.task_info['task_id'] = task_id
+        
+    def set_create_task_timestamp(self, create_task_timestamp):
+        self.task_info['create_task_timestamp'] = create_task_timestamp
+        
+    def set_end_task_timestamp(self, end_task_timestamp):
+        self.task_info['end_task_timestamp'] = end_task_timestamp
+        
+    def set_reward(self, reward):
+        self.task_info['reward'] = reward
+        
+    def set_pair(self, pair):
+        self.task_info['pair'] = pair
+        
+    def set_buy_price(self, buy_price):
+        self.task_info['buy_price'] = buy_price
+        
+    def set_sell_price(self, sell_price):
+        self.task_info['sell_price'] = sell_price
+        
+    def set_position_size(self, position_size):
+        self.task_info['position_size'] = position_size
+        
+    def set_profit(self, profit):
+        self.task_info['profit'] = profit
+        
+    def set_buy_order_id(self, buy_order_id):
+        self.task_info['buy_order_id'] = buy_order_id
+        
+    def set_take_profit_order_id(self, take_profit_order_id):
+        self.task_info['take_profit_order_id'] = take_profit_order_id
+        
+    def set_stop_loss_order_id(self, stop_loss_order_id):
+        self.task_info['stop_loss_order_id'] = stop_loss_order_id
+        
+    def set_status(self, status):
+        self.task_info['status'] = status
+        
+    def set_next_step(self, next_step):
+        self.task_info['next_step'] = next_step
 
-    def get_symbol(self):
-        return self.task_info["symbol"]
+    def get_task_id(self):
+        return self.task_info['task_id']
 
-    def get_quantity(self):
-        return self.task_info["quantity"]
+    def get_create_task_timestamp(self):
+        return self.task_info['create_task_timestamp']
 
-    def get_take_profit_val(self):
-        return self.task_info["take_profit_val"]
+    def get_end_task_timestamp(self):
+        return self.task_info['end_task_timestamp']
 
-    def get_stop_loss_val(self):
-        return self.task_info["stop_loss_val"]
+    def get_reward(self):
+        return self.task_info['reward']
 
-    def get_entry_price_val(self):
-        return self.task_info["entry_price_val"]
+    def get_pair(self):
+        return self.task_info['pair']
 
-    def get_position_size_val(self):
-        return self.task_info["position_size_val"]
+    def get_buy_price(self):
+        return self.task_info['buy_price']
 
-    def get_risk_percent_val(self):
-        return self.task_info["risk_percent_val"]
+    def get_sell_price(self):
+        return self.task_info['sell_price']
+
+    def get_position_size(self):
+        return self.task_info['position_size']
+
+    def get_profit(self):
+        return self.task_info['profit']
+
+    def get_buy_order_id(self):
+        return self.task_info['buy_order_id']
+
+    def get_take_profit_order_id(self):
+        return self.task_info['take_profit_order_id']
+
+    def get_stop_loss_order_id(self):
+        return self.task_info['stop_loss_order_id']
 
     def get_status(self):
-        return self.task_info["status"]
+        return self.task_info['status']
 
     def get_next_step(self):
-        return self.task_info["next_step"]
+        return self.task_info['next_step']
 
-    def get_timestamp(self):
-        return self.task_info["timestamp"]
+
+
+
+
 
