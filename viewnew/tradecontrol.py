dd5ea405af674328e7d6280a03b63f0ca866db5d
inspiron5559 <bryogenin_haul@aleeas.com>
2021-01-20 04:54:08 +0800

    separate trade options and summary class

--- viewnew/tradecontrol.py
+++ viewnew/tradecontrol.py
@@ -1,24 +1,29 @@
 import tkinter as tk
 
+tradeControlWidth = 500
+
 class TradeControl(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
 
-        self.parent = parent
+        trade_option_frame = TradeOptions(self)
+        trade_summary_frame = TradeSummary(self)
 
-        width = 500
-
-        # trade options
-        frame1 = tk.Frame(self)
-        trade_option_frame = tk.LabelFrame(frame1, text="Options", width=width)
-        trade_option_frame.pack(fill=tk.Y, expand=1)
+        trade_option_frame.pack(fill=tk.Y, expand=1, padx=10, pady=10)
+        trade_summary_frame.pack(fill=tk.Y, expand=1, padx=10, pady=10)
 
 
-        # trade summary
-        frame2 = tk.Frame(self)
-        trade_summary_frame = tk.LabelFrame(frame2, text="Summary", width=width)
-        trade_summary_frame.pack(fill=tk.Y, expand=1)
+class TradeOptions(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
 
-        frame1.pack(fill=tk.Y, expand=1, padx=10, pady=10)
-        frame2.pack(fill=tk.Y, expand=1, padx=10, pady=10)
+        label_frame = tk.LabelFrame(self, text="Options", width=tradeControlWidth)
+        label_frame.pack(fill=tk.Y, expand=1)
 
+
+class TradeSummary(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        label_frame = tk.LabelFrame(self, text="Summary", width=tradeControlWidth)
+        label_frame.pack(fill=tk.Y, expand=1)
