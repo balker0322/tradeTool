10ae6dcc1b89f36e23105701bee565c1522ff653
balker0322 <balker0322@gmail.com>
2021-06-10 07:47:40 +0800

    add component

--- rmview/components.py
+++ rmview/components.py
@@ -1,31 +1,40 @@
 import tkinter as tk
 
+class EntryComponent_A(tk.Frame):
+    pass
 
 class EntryComponent(tk.Frame):
     
     def __init__(self, parent, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
+        tk.Frame.__init__(self, parent)
+        # super().__init__(parent, *args, **kwargs)
 
-        label = tk.Label(self, text='label', anchor=tk.W)
+        label = tk.Label(self, text=kwargs['label'], anchor=tk.W)
         label.pack(fill=tk.X)
         
         left_frame = tk.Frame(self)
         left_frame.pack(side=tk.LEFT, fill=tk.X, expand=True)
 
-        entry = tk.Entry(left_frame)
+        self.entry_variable = tk.StringVar()
+        entry = tk.Entry(left_frame, textvariable=self.entry_variable)
         entry.pack(fill=tk.X, expand=True)
 
-        button_list = ['button1', 'button2', 'button3', 'button4']
         buttons = []
 
-        for button_item in button_list:
-            button = tk.Button(left_frame, text=button_item)
+        for button_item in kwargs['button_list']:
+            button = tk.Button(left_frame, text=button_item['text'], command=button_item['command'])
             button.pack(side=tk.LEFT, fill=tk.X, expand=True)
             buttons.append(button)
             
         right_frame = tk.Frame(self)
         right_frame.pack(side=tk.LEFT, fill=tk.Y)
 
-        submit_button = tk.Button(right_frame, text='submit')
+        submit_button = tk.Button(right_frame, text=kwargs['submit_button']['text'], command=kwargs['submit_button']['command'])
         submit_button.pack(side=tk.TOP)
+    
+    def set_entry(self, set_var):
+        self.entry_variable.set(set_var)
+
+    def get_entry(self):
+        return self.entry_variable.get()
 
--- rmview/mainapplication.py
+++ rmview/mainapplication.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from .components import EntryComponent
 
+def dummy_func():
+    pass
+
 class MainApplication(tk.Tk):
 
     def __init__(self):
@@ -8,5 +11,38 @@ class MainApplication(tk.Tk):
 
         # self.geometry("1500x900")
 
-        sample_comp = EntryComponent(self)
-        sample_comp.pack(fill=tk.BOTH)
+        self.sample_comp = EntryComponent(
+            parent=self,
+            label='sample label',
+            entry_on_change=dummy_func,
+            submit_button={
+                'text':'submit',
+                'command':dummy_func
+            },
+            button_list=[
+                {
+                    'text':'button1',
+                    'command':self.set_variable
+                },
+                {
+                    'text':'button2',
+                    'command':dummy_func
+                },
+                {
+                    'text':'button3',
+                    'command':dummy_func
+                },
+                {
+                    'text':'button4',
+                    'command':dummy_func
+                },
+                {
+                    'text':'button5',
+                    'command':dummy_func
+                },
+            ]
+        )
+        self.sample_comp.pack(fill=tk.BOTH)
+
+    def set_variable(self):
+        self.sample_comp.set_entry('hello')
